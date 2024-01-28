c83060879a52f2313afc17e016d20c570aa891da
Kenzzer <kenzzer@users.noreply.github.com>
2024-01-28 18:18:38 +0100

    AMBuildifying
    remove unnecessary files
    
    Move the git patching into ambuild
    
    move lss to a patch
    
    Add windows compilation support
    
    remove breakpad.bat
    
    move postlink libs

---
+++ third_party/cwd_cmd.py
@@ -0,0 +1,20 @@
+# vim: set ts=2 sw=2 tw=99 noet ft=python:
+import subprocess, sys  
+
+argv = sys.argv[3:]
+cwd = sys.argv[1]
+print('CMD: ' + ' '.join(argv))
+print('CWD: ' + cwd)
+print('OUT: ' + sys.argv[2])
+open(sys.argv[2], 'a').close()
+
+args = { 'args': ' '.join(argv),
+          'stdout': subprocess.PIPE,
+          'stderr': subprocess.PIPE,
+          'shell':  True,
+          'cwd': cwd }
+p = subprocess.Popen(**args)
+stdout, stderr = p.communicate()
+if p.returncode != 0:
+  raise Exception('terminated with non-zero exitcode {0}'.format(p.returncode))
+sys.exit(p.returncode)
